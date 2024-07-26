3ad98eff1679fabd75dfdca93a4c7db407fe3764
DevinUpdh <devin.updh@gmail.com>
2024-07-26 15:11:12 +0545

    board and the pieces are generated

--- Chess_main.py
+++ Chess_main.py
@@ -1,15 +1,75 @@
 import pygame as p
 import Chess_engine
+import sys
 
-WIDTH =  HEIGHT = 512
-DIMENSION = 8  # 8x8 chess board
-SQ_SIZE = HEIGHT // DIMENSION
-MAX_FPS=15 # for animation
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+
+SQUARE_SIZE = HEIGHT // DIMENSION
+
+MAX_FPS = 15
+
 IMAGES = {}
 
-def loadImages():
-    pieces = ["wp", "wR", "wB", "wQ", "wK", "wN", "bp", "bR", "bB", "bQ", "bK", "bN"]
-    for piece in pieces:
-        # scaled the images according to the board size
-        IMAGES[piece] = p.transform.scale(p.image.load(f"images/+{piece}.png"), (SQ_SIZE, SQ_SIZE))
 
+def loadImages():
+    '''
+    Initialize a global directory of images.
+    This will be called exactly once in the main.
+    '''
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(f"images/{piece}.png" ), (SQUARE_SIZE, SQUARE_SIZE))
+
+
+def main():
+    '''
+    The main driver for our code.
+    This will handle user input and updating the graphics.
+    '''
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    game_state = Chess_engine.GameState()
+    loadImages()  # do this only once before while loop
+
+    running = True
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+
+        drawGameState(screen, game_state)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawGameState(screen, game_state):
+    '''
+    Responsible for all the graphics within current game state.
+    '''
+    drawBoard(screen)  # draw squares on the board
+    # add in piece highlighting or move suggestions (later)
+    drawPieces(screen, game_state.board)  # draw pieces on top of those squares
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            color = colors[((row + column) % 2)]
+            p.draw.rect(screen, color, p.Rect(column * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+
+if __name__ == "__main__":
+    main()
